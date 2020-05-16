16eacbb80ac3c8ad4149976b890e6416df67e9c9
Stanley Huang <huang.stanley050@gmail.com>
2020-05-16 11:22:14 +1000

    start

---
+++ README.md
@@ -0,0 +1,11 @@
+# gatsby-paginated-blog
+
+Gatsby starter for creating a blog with pagination
+
+Install this starter by running from your CLI:
+
+`npx gatsby new my-paginated-blog https://github.com/NickyMeuleman/gatsby-paginated-blog`
+
+## Running in development
+
+`npm run develop`
---
+++ src/components/Layout.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import { rhythm, scale } from '../utils/typography'
+
+class Layout extends React.Component {
+  render() {
+    const { location, title, children } = this.props
+    const isRootPath = location.pathname === `${__PATH_PREFIX__}/`
+    const pageNumber = location.pathname
+      .split('/')
+      .filter(Boolean)
+      .pop()
+    const isPaginatedPath = pageNumber && Boolean(pageNumber.match(/^[0-9]+$/))
+    let header
+
+    if (isRootPath || isPaginatedPath) {
+      header = (
+        <h1
+          style={{
+            ...scale(1.5),
+            marginBottom: rhythm(1.5),
+            marginTop: 0,
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+            to={`/`}
+          >
+            {title}
+          </Link>
+        </h1>
+      )
+    } else {
+      header = (
+        <h3
+          style={{
+            fontFamily: `Montserrat, sans-serif`,
+            marginTop: 0,
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+            to={`/`}
+          >
+            {title}
+          </Link>
+        </h3>
+      )
+    }
+    return (
+      <div
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          maxWidth: rhythm(24),
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        }}
+      >
+        {header}
+        {children}
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
+    )
+  }
+}
+
+export default Layout
