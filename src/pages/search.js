af56bd0a991ce27844c2b20cfb7b34e9ba4b80f6
Stanley Huang <infamous_godhand@yahoo.com>
2020-06-18 19:45:57 +1000

    getting search result loaded

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../components/Layout';
 import SearchForm from '../components/SearchForm';
 import SearchResult from '../components/SearchResult';
 
 const Search = ({ location, data }) => {
+  const [results, setResults] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      let result = await window.__LUNR__.__loaded;
+      let refs = result.en.index.search('code');
+      let posts = refs.map(({ ref }) => result.en.store[ref]);
+      console.log(posts);
+      setResults(posts);
+    };
+    if (window.__LUNR__) {
+      fetchPosts();
+    }
+  }, []);
+
   return (
     <Layout location={location} title={data.site.siteMetadata.title}>
       <SearchForm />
