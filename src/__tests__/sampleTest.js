bae4f428d0e10d1d70285bd8f238bfcbd485ba38
Stanley Huang <infamous_godhand@yahoo.com>
2020-10-18 10:26:14 +1100

    setting up for chrome dev tool test

---
+++ src/__tests__/sampleTest.js
@@ -0,0 +1,8 @@
+export const add = (a, b) => a + b;
+export const minus = (a, b) => b - a;
+export const divide = (a, b) => b / a;
+
+export const mult = (a, b) => {
+  const result = a * b;
+  return result;
+};
