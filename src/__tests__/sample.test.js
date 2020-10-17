8ca2b2c592c71d0ef990822d2365ea3f18025c3b
Stanley Huang <infamous_godhand@yahoo.com>
2020-10-18 10:35:24 +1100

    working under chrome dev tool

---
+++ src/__tests__/sample.test.js
@@ -0,0 +1,7 @@
+import { add, mult, divide, minus } from './sampleTest';
+
+test('it should return the result of 5', () => {
+  const result = add(1, 4);
+  debugger;
+  expect(result).toBe(5);
+});
